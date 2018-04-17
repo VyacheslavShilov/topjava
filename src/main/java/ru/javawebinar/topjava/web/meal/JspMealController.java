77cc3b7382daadfab772d8e5fa57a16ba9aab288
Vyacheslav Shilov <rredlis@gmail.com>
2018-04-17 21:54:35 +0500

    HW01 3 сделал локализацию meals.jsp и @GetMapping("/meals")

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -1,45 +1,35 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.util.ValidationUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
+@Controller
 public class JspMealController {
 
     @Autowired
     private MealService service;
 
-    @PostMapping("/meals")
-    public String meals(HttpServletRequest request) throws UnsupportedEncodingException {
-        request.setCharacterEncoding("UTF-8");
-        Meal meal = new Meal(
-                LocalDateTime.parse(request.getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.parseInt(request.getParameter("calories")));
-
-        if (request.getParameter("id").isEmpty()) {
-            service.create(meal, AuthorizedUser.id());
-        } else {
-            service.update(meal, AuthorizedUser.id());
-        }
+    @GetMapping("/meals")
+    public String getAllMeal(Model model){
+       model.addAttribute("meals", MealsUtil.getWithExceeded(service.getAll(AuthorizedUser.id()), AuthorizedUser.getCaloriesPerDay()));
         return "meals";
     }
 
-    @GetMapping("/meals/delete")
-    public String deleateMeal(HttpServletRequest request){
-        int id = getId(request);
-        service.delete(id,  AuthorizedUser.id());
-        return "meals";
-    }
 
     private int getId(HttpServletRequest request) {
         String paramId = Objects.requireNonNull(request.getParameter("id"));
